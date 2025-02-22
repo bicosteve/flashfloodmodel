5543d438b59810e736950e87869d9a57c869a7c4
bsteve <bicosteve4@gmail.com>
2025-02-22 06:35:20 +0300

    integrated ui with app

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 import pickle
 import math as math
-import traceback
 
 
 import numpy as np
-from flask import Flask, flash, request, jsonify, render_template, redirect, url_for
+from flask import Flask, flash, request, render_template, redirect, url_for
 
 from helpers.helpers import (
-    validate_erosion_degree_value,
-    validate_continous_variables,
     load_env_vars,
+    validate_erosion_degree,
+    validate_continous_variables,
 )
 from categories.categorize import CategorizeErosionDegree
-
-
-from database.db import connection
+from database.db import db_connection
 
 
 app = Flask(__name__)
@@ -30,175 +27,93 @@ except (pickle.UnpicklingError, TypeError) as e:
     print(f"Error loading model: {e}")
 
 
-mysql = connection(
+mysql = db_connection(
     env_vars["HOST"], env_vars["USER"], env_vars["PASSWORD"], env_vars["DATABASE"]
 )
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
-    return jsonify({"msg": "This is test"})
-
-
-@app.route("/api", methods=["GET", "POST"])
-def predict_api():
-    try:
-        data = request.get_json()
-        erosion_degree = data["erosion-degree"]
-        rainfall_amount = data["rainfall"]
-        river_discharge = data["river-discharge"]
-        soil_moisture_content = data["soil-moisture"]
-
-        isValid = validate_erosion_degree_value(erosion_degree)
-        if not isValid:
-            return jsonify({"msg": "Invalid erosion degree"})
-        isValid = validate_erosion_degree_value(erosion_degree)
-        if not isValid:
-            return jsonify({"message": f"Erosion degree value is not valid"})
-
-        isValid = validate_continous_variables(
-            soil_moisture_content,
-            rainfall_amount,
-            river_discharge,
-        )
-
-        if not isValid:
-            return jsonify({"message": "Invalid continues variables"})
-
-        Erosion_degree = CategorizeErosionDegree(erosion_degree)
-        Soil_Moisture = float(soil_moisture_content)
-        Rainfall_mm = float(rainfall_amount)
-        River_Discharge_m3s = float(river_discharge)
-        Soil_Moisture = float(soil_moisture_content)
-        Rainfall_mm = float(rainfall_amount)
-
-        features = [
-            Erosion_degree,
-            Soil_Moisture,
-            Rainfall_mm,
-            River_Discharge_m3s,
-        ]
-
-        final_features = np.array([features])
-
-        rf_res = rf_model.predict(final_features)
-        svc_res = svc_model.predict(final_features)
-        lr_res = lr_model.predict(final_features)
-
-        rf_output = round(rf_res[0].item(), 2)
-        svc_output = round(svc_res[0].item(), 2)
-        lr_output = round(lr_res[0].item(), 2)
-
-        average = rf_output + svc_output + lr_output
-        predict_text = ""
-
-        accuracy = average / 3  # -----> maintain the result of each.NB 3 is also ok
-
-        if accuracy == 1:
-            predict_text = "High chances of flash floods"
-        elif accuracy > 0.5:
-            predict_text = "Medium chances of flash floods"
-        else:
-            predict_text = "Low chances of flash floods"
-
-        return jsonify(
-            {
-                "random_forest": rf_output,
-                "svc_model": svc_output,
-                "lr_model": lr_output,
-                "accuracy": accuracy,
-                "Prediction": predict_text,
-            }
-        )
-    except Exception as e:
-        error_details = {
-            "error_type": type(e).__name__,  # Name of the exception class
-            "error_message": str(e),  # Error message
-            "stack_trace": traceback.format_exc(),  # Full stack trace as a string
-        }
-        print("all")
-        return jsonify({"msg": error_details})
-
-
-@app.route("/add", methods=["POST"])
 def predict():
-    conn = None
-    cursor = None
-    try:
-        if request.method == "POST":
-            erosion_degree = request.form["erosion-degree"]
-            soil_moisture_content = request.form["soil-moisture"]
-            rainfall_amount = request.form["rainfall"]
-            river_discharge = request.form["river-discharge"]
+    if request.method == "POST":
+        soil_moisture_content = request.form["soil-moisture"]
+        rainfall_amount = request.form["rainfall"]
+        river_discharge = request.form["river-discharge"]
+        erosion_degree = request.form["erosion-degree"]
 
-            # Validate the inputs
-            is_valid_erosion_degree = validate_erosion_degree_value(erosion_degree)
-            is_valid_continuous_vars = validate_continous_variables(
-                soil_moisture_content, rainfall_amount, river_discharge
-            )
+        is_valid_erosion_degree = validate_erosion_degree(erosion_degree)
+        is_valid_continuous_vars = validate_continous_variables(
+            soil_moisture_content, rainfall_amount, river_discharge
+        )
 
-            if is_valid_erosion_degree and is_valid_continuous_vars:
-                Erosion_degree = CategorizeErosionDegree(erosion_degree)
-                Soil_Moisture = float(soil_moisture_content)
-                Rainfall_mm = float(rainfall_amount)
-                River_Discharge_m3s = float(river_discharge)
-                Soil_Moisture = float(soil_moisture_content)
-                Rainfall_mm = float(rainfall_amount)
+        if is_valid_erosion_degree and is_valid_continuous_vars:
+            Erosion_degree = CategorizeErosionDegree(erosion_degree)
+            Soil_Moisture = float(soil_moisture_content)
+            Rainfall_mm = float(rainfall_amount)
+            River_Discharge_m3s = float(river_discharge)
 
-                features = [
-                    Erosion_degree,
-                    Soil_Moisture,
-                    Rainfall_mm,
-                    River_Discharge_m3s,
-                ]
+            features = [
+                Erosion_degree,
+                Soil_Moisture,
+                Rainfall_mm,
+                River_Discharge_m3s,
+            ]
 
-                final_features = np.array([features])
+            final_features = np.array([features])
 
-                rf_res = rf_model.predict(final_features)
-                svc_res = svc_model.predict(final_features)
-                lr_res = lr_model.predict(final_features)
+            rf_res = rf_model.predict(final_features)
+            svc_res = svc_model.predict(final_features)
+            lr_res = lr_model.predict(final_features)
 
-                rf_output = round(rf_res[0].item(), 2)
-                svc_output = round(svc_res[0].item(), 2)
-                lr_output = round(lr_res[0].item(), 2)
+            rf_output = round(rf_res[0].item(), 2)
+            svc_output = round(svc_res[0].item(), 2)
+            lr_output = round(lr_res[0].item(), 2)
 
-                average = rf_output + svc_output + lr_output
-                predict_text = ""
+            average = rf_output + svc_output + lr_output
 
-                accuracy = average / 3
+            accuracy = average / 3
 
-                if accuracy == 1:
-                    predict_text = "High chances of flash floods"
-                elif accuracy > 0.5:
-                    predict_text = "Medium chances of flash floods"
-                else:
-                    predict_text = "High chances of flash floods"
+            if accuracy == 1:
+                predict_text = "High chances of flash floods"
+            elif accuracy > 0.5:
+                predict_text = "Medium chances of flash floods"
+            else:
+                predict_text = "High chances of flash floods"
 
-                q = """
-                    INSERT INTO flood_data(prediction_text,lr_model,rf_model,svc_model,accuracy) VALUES(%s,%s,%s,%s,%s)
-                    """
-                data = (predict_text, lr_model, rf_model, svc_model, accuracy)
+            cursor = mysql.cursor()
 
-                conn = mysql.connect()
-                cursor = conn.cursor()
+            q = """
+                INSERT INTO flood_data(prediction_text,lr_model,rf_model,svc_model,accuracy) VALUES(%s,%s,%s,%s,%s)
+                """
+            data = (predict_text, lr_output, rf_output, svc_output, accuracy)
 
-                cursor.execute(q, data)
-                conn.commit()
+            print(data)
 
-                flash("Data added successfully!")
-                return redirect(url_for("index"))
+            cursor.execute(q, data)
+            mysql.commit()
+
+            flash("Data added successfully!")
+            return redirect("/results")
         else:
-            results = cursor.execute("SELECT * FROM flood_data")
-            if results > 0:
-                flood_details = cursor.fetchall()
-                return render_template("index.html", details=flood_details)
-            return render_template("index.html")
+            print("invalid results")
+    return render_template("index.html")
+
+
+@app.route("/results")
+def results():
+    try:
+        cursor = mysql.cursor()
+        rows = cursor.execute("SELECT * FROM flood_data")
+        if rows > 0:
+            flood_details = cursor.fetchall()
+            return render_template("results.html", details=flood_details)
+        return "An error occurred while fetching results"
     except Exception as e:
-        print(f"An error {str(e)}")
+        print(e)
     finally:
         cursor.close()
-        conn.close()
+        mysql.close()
 
 
 if __name__ == "__main__":
+    app.secret_key = "you will never know"
     app.run(port=5500, debug=True)
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,84 +1,6 @@
 import toml
 
 
-def validate_erosion_degree_value(value):
-    erosion_degree_accepted_values = ["E", "M", "S", "V"]
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-
-    return value in erosion_degree_accepted_values
-
-
-def validate_slope_position_value(value):
-    slope_position_accepted_values = ["A", "D", "H", "L", "M"]
-    """
-    1. Cleans the slope position value,
-    2. validates if it is in the accepted values and return a boolean
-    """
-
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-
-    return value in slope_position_accepted_values
-
-
-def validate_surface_stoniness_value(value):
-    surface_stoniness_accepted_values = ["A", "C", "D", "F", "M", "N", "V"]
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-
-    return value in surface_stoniness_accepted_values
-
-
-def validate_affected_area_value(value):
-    area_affected_accepted_values = ["1", "2", "3", "4", "5"]
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-
-    return value in area_affected_accepted_values
-
-
-def validate_sensitivity_to_capping_value(value):
-    sensitivity_to_capping_accepted_values = ["M", "N", "S", "W"]
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-
-    return value in sensitivity_to_capping_accepted_values
-
-
-def validate_land_use_type_value(value):
-    land_use_type_accepted_values = ["Forest", "Agriculture", "Urban", "Wetland"]
-
-    clean_string = value.strip()
-    if len(clean_string) < 1:
-        return False
-
-    clean_string = clean_string.upper()
-    return value in land_use_type_accepted_values
-
-
-def validate_continous_variables(soil, rainfall, river):
-    if soil < 1 and rainfall < 1 and river < 1:
-        return False
-
-    return True
-
-
 def load_env_vars(file_path):
     try:
         with open(file_path, "r") as f:
@@ -93,3 +15,22 @@ def load_env_vars(file_path):
         env_vars[key] = str(value)
 
     return env_vars
+
+
+def validate_erosion_degree(value):
+    accepted_values = ["E", "M", "S", "V"]
+    clean_string = value.strip()
+    if len(clean_string) < 1 and value not in accepted_values:
+        return False
+    return True
+
+
+def validate_continous_variables(soil, rainfall, river):
+    soil = int(soil)
+    rainfall = int(rainfall)
+    river = int(river)
+
+    if soil < 1 and rainfall < 1 and river < 1:
+        return False
+
+    return True
